5eba97a50dcc8b7cb604d61267836ad2cd7977f1
Abgespie <molch-slava@yandex.ru>
2023-04-01 22:01:20 +0300

    second

--- js/script.js
+++ js/script.js
@@ -1,6 +1,27 @@
-function startg() {
-    hideItem(document.getElementsByClassName('start')[0]);
-    showItem(document.getElementsByClassName('main')[0]);
+//toggle functions
+
+var prev = 1;
+
+function start() {
+    console.log(prev);
+    switch(prev) {
+        case 1:
+            hideItem(document.getElementsByClassName('start')[0]);
+            showItem(document.getElementsByClassName('main')[0]);
+            showItem(document.getElementsByClassName('exit')[0]);
+            break;
+        case 2:
+            hideItem(document.getElementsByClassName('start')[0]);
+            showItem(document.getElementsByClassName('videos')[0]);
+            showItem(document.getElementsByClassName('exit')[0]);
+            break;
+        case 3:
+            hideItem(document.getElementsByClassName('start')[0]);
+            showItem(document.getElementsByClassName('test')[0]);
+            showItem(document.getElementsByClassName('exit')[0]);
+            break;
+    }
+    
 }
 function tovideos() {
     if(document.getElementsByClassName('main')[0].classList.contains('block-hidden')) {
@@ -27,11 +48,31 @@ function tomain() {
     if(document.getElementsByClassName('test')[0].classList.contains('block-hidden')) {
         hideItem(document.getElementsByClassName('videos')[0]);
         showItem(document.getElementsByClassName('main')[0]);
-        console.log(document.getElementsByClassName('test')[0].classList.contains('block-hidden'));
     }
     else {
         hideItem(document.getElementsByClassName('test')[0]);
         showItem(document.getElementsByClassName('main')[0]);
-        console.log(document.getElementsByClassName('test')[0].classList.contains('block-hidden'));
+    }
+}
+function tostart() {
+    if(document.getElementsByClassName('videos')[0].classList.contains('block-hidden') && document.getElementsByClassName('main')[0].classList.contains('block-hidden')) {
+        hideItem(document.getElementsByClassName('test')[0]);
+        hideItem(document.getElementsByClassName('exit')[0]);
+        showItem(document.getElementsByClassName('start')[0]);
+        prev = 3;
+    }
+    else {
+        if(document.getElementsByClassName('test')[0].classList.contains('block-hidden') && document.getElementsByClassName('videos')[0].classList.contains('block-hidden')) {
+            hideItem(document.getElementsByClassName('main')[0]);
+            hideItem(document.getElementsByClassName('exit')[0]);
+            showItem(document.getElementsByClassName('start')[0]);
+            prev = 1;
+        }
+        else {
+            hideItem(document.getElementsByClassName('videos')[0]);
+            hideItem(document.getElementsByClassName('exit')[0]);
+            showItem(document.getElementsByClassName('start')[0]);
+            prev = 2;
+        }
     }
 }
